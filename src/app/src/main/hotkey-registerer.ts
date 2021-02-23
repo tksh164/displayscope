cd55ffaac60b878556b1e16b2b4c2bb6019c284d
Takeshi Katano <takatano@outlook.com>
2021-02-23 15:34:22 +0900

    Update hotkey code

--- src/app/src/main/hotkey-registerer.ts
+++ src/app/src/main/hotkey-registerer.ts
@@ -3,10 +3,11 @@ import { setMouseCursorPosition } from "@/main/mouse-cursor-setter";
 
 export const HOTKEY_MOVE_MOUSE_CURSOR_TO_APP_WINDOW = "Shift+Escape";
 
-let win: BrowserWindow;
-export function registerMoveMouseCursorToAppWindowHotkey(browserWindow: BrowserWindow): boolean {
-  win = browserWindow;
+// Retain the app window reference.
+let appWindow: BrowserWindow;
 
+export function registerMoveMouseCursorToAppWindowHotkey(win: BrowserWindow): boolean {
+  appWindow = win;
   // Register a hotkey to move the mouse cursor to on app window from the screen.
   return globalShortcut.register(HOTKEY_MOVE_MOUSE_CURSOR_TO_APP_WINDOW, moveMouseCursorToAppWindowArea);
 }
@@ -17,8 +18,14 @@ export function unregisterMoveMouseCursorToAppWindowHotkey(): void {
 }
 
 function moveMouseCursorToAppWindowArea(): void {
-  const winScreenRect = screen.dipToScreenRect(win, win!.getBounds());
-  const posX = Math.floor(winScreenRect.width / 2) + winScreenRect.x;
-  const posY = Math.floor(winScreenRect.height / 2) + winScreenRect.y;
+  const [posX, posY] = calcCenterPositionInWindow(appWindow);
   setMouseCursorPosition(posX, posY);
+}
+
+function calcCenterPositionInWindow(win: BrowserWindow): [number, number] {
+  const winScreenRect = screen.dipToScreenRect(win, win!.getBounds());
+  return [
+    Math.floor(winScreenRect.width / 2) + winScreenRect.x,
+    Math.floor(winScreenRect.height / 2) + winScreenRect.y
+  ];
 }
