34e6705669f8dcea7dfb84d217732fd04c671ddf
Takeshi Katano <takatano@outlook.com>
2021-03-31 00:34:50 +0900

    Update UI

--- src/app/src/views/ScreenSelect.vue
+++ src/app/src/views/ScreenSelect.vue
@@ -1,57 +1,55 @@
 <template>
-  <el-container class="container">
-    <el-header style="padding: 1rem; height: auto;">
-      <div class="function-area">
-        <div class="function-area-item">
-          <el-switch v-model="alwaysOnTop"
-            active-text="Always on top"
-            @change="changeAlwasyOnTop"></el-switch>
+  <div class="screen-select-wrapper">
+    <el-container class="container">
+      <el-header style="padding: 1rem; height: auto;">
+        <div class="function-area">
+          <div class="function-area-item">
+            <el-switch v-model="alwaysOnTop" active-text="Always on top" @change="changeAlwasyOnTop"></el-switch>
+          </div>
+          <div class="function-area-item">
+            <el-button type="primary" circle icon="el-icon-refresh" @click="refreshScreenMetadataList"></el-button>
+          </div>
         </div>
-        <div class="function-area-item">
-          <el-button type="primary"
-            circle
-            icon="el-icon-refresh"
-            @click="refreshScreenMetadataList"></el-button>
-        </div>
-      </div>
-    </el-header>
-    <el-main style="padding: 1rem;">
-    <transition-group name="list-item-transition"
-      appear
-      tag="div"
-      class="screen-list">
-      <screen-item v-for="screenItem in screenItems"
-        :key="screenItem.id"
-        :screenId="screenItem.id"
-        :centerPoint="screenItem.centerPoint"
-        :screenName="screenItem.name"
-        :screenDescription="screenItem.description"
-        :thumbnailUrl="screenItem.thumbnailDataUri"></screen-item>
-    </transition-group>
-    </el-main>
-  </el-container>
+      </el-header>
+      <el-main style="padding: 1rem;">
+        <transition-group name="list-item-transition" appear tag="div" class="screen-list">
+          <screen-item v-for="screenItem in screenItems"
+            :key="screenItem.id"
+            :screenId="screenItem.id"
+            :centerPoint="screenItem.centerPoint"
+            :screenName="screenItem.name"
+            :screenDescription="screenItem.description"
+            :thumbnailUrl="screenItem.thumbnailDataUri"></screen-item>
+        </transition-group>
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
 <style>
-.container {
+.screen-select-wrapper {
   height: 100%;
 }
 
-.function-area {
+.screen-select-wrapper .container {
+  height: 100%;
+}
+
+.screen-select-wrapper .function-area {
   display: flex;
   justify-content: flex-end;
   align-items: center;
 }
 
-.function-area-item {
+.screen-select-wrapper .function-area-item {
   margin-left: 2rem;
 }
 
-.function-area-item .el-switch__label {
+.screen-select-wrapper .function-area-item .el-switch__label {
   color: #cccccc;
 }
 
-.screen-list {
+.screen-select-wrapper .screen-list {
   width: 100%;
   height: 100%;
   display: flex;
@@ -60,13 +58,13 @@
   align-items: center;
 }
 
-.list-item-transition-enter-active,
-.list-item-transition-leave-active {
+.screen-select-wrapper .list-item-transition-enter-active,
+.screen-select-wrapper .list-item-transition-leave-active {
   transition: opacity 0.2s;
 }
 
-.list-item-transition-enter,
-.list-item-transition-leave-to {
+.screen-select-wrapper .list-item-transition-enter,
+.screen-select-wrapper .list-item-transition-leave-to {
   opacity: 0;
 }
 </style>
