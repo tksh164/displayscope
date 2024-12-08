45b2867ced0dfb574d59900f2b86e419a193cc28
Takeshi Katano <takatano@outlook.com>
2024-12-08 09:46:47 +0900

    Fix code formatting

--- src/app-react/src/main/appWindowSize.ts
+++ src/app-react/src/main/appWindowSize.ts
@@ -4,6 +4,6 @@ export function getInitialAppWindowSize(): [number, number] {
   const workAreaSize = screen.getPrimaryDisplay().workAreaSize;
   return [
     Math.floor(workAreaSize.width * 0.8),
-    Math.floor(workAreaSize.height * 0.8)
+    Math.floor(workAreaSize.height * 0.8),
   ];
 }
