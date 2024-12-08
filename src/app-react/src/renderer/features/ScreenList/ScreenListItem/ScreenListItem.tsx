279bd26e39ee137e7629c6ae0d7816f295a103eb
Takeshi Katano <takatano@outlook.com>
2024-12-08 11:50:24 +0900

    Change code style

--- src/app-react/src/renderer/features/ScreenList/ScreenListItem/ScreenListItem.tsx
+++ src/app-react/src/renderer/features/ScreenList/ScreenListItem/ScreenListItem.tsx
@@ -3,10 +3,14 @@ import { ScreenSpec } from "src/main/types/screenSpec";
 import "./ScreenListItem.css";
 
 export default function ScreenListItem(props: { screenSpec: ScreenSpec }) {
+  const primaryText = props.screenSpec.display.isPrimary ? "Primary, " : "";
+  const resolutionText = props.screenSpec.display.bounds.width + " x " + props.screenSpec.display.bounds.height;
+  const scaleFactorText = props.screenSpec.display.scaleFactor * 100 + "%";
+
   return (
     <div className="screen-list-item">
       <Text block={true} size={500} weight="semibold" className="screen-name">{props.screenSpec.name}</Text>
-      <Text block={true} size={300} className="screen-description">{props.screenSpec.display.isPrimary ? "Primary, " : "" }{props.screenSpec.display.bounds.width} x {props.screenSpec.display.bounds.width}, {props.screenSpec.display.scaleFactor * 100}%</Text>
+      <Text block={true} size={300} className="screen-description">{primaryText}{resolutionText}, {scaleFactorText}</Text>
       <Image className="screen-thumbnail" shape="rounded" src={props.screenSpec.thumbnailDataUri} />
     </div>
   );
