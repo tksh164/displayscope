2adfe2f0681c43096e7832cbf7cf315168b757ca
Takeshi Katano <takatano@outlook.com>
2025-01-05 14:02:40 +0900

    Comment-out logging

--- src/app/src/renderer.ts
+++ src/app/src/renderer.ts
@@ -29,4 +29,4 @@
 import "./renderer/index.css";
 import "./renderer/index";
 
-console.log('👋 This message is being logged by "renderer.ts", included via Vite');
+//console.log('👋 This message is being logged by "renderer.ts", included via Vite');
